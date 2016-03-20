da96eaa634ceea10320fd80f750c69a8ab1d2b4c
Eugene K <eugene_kotov@hotmail.com>
2016-03-20 13:29:49 +0500

    Создание пользователя в бд

--- init.sh
+++ init.sh
@@ -5,4 +5,12 @@ sudo /etc/init.d/nginx restart
 
 #gunicorn conf
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/ask
-sudo /etc/init.d/gunicorn restart 
+sudo /etc/init.d/gunicorn restart
+
+#database conf
+mysql -uroot -e "CREATE DATABASE ASK"
+mysql -uroot -e "CREATE USER 'sa'@'localhost' IDENTIFIED BY 'sa'"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON ASK.* TO 'sa'@'localhost'"
+
+
+python /home/box/web/ask/manage.py syncdb
