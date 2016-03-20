f70dc1500aba2db7f3b43d82ad81e173455276ca
Eugene K <eugene_kotov@hotmail.com>
2016-03-20 12:05:39 +0500

    Web frameworks, 1st ex

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -7,6 +7,12 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'qa.views.test', name='home'),
+    url(r'^login/.*$', 'qa.views.test', name='login'),
+    url(r'^signup/.*$', 'qa.views.test', name='signup'),
+    url(r'^question/\d+/$', 'qa.views.test', name='question'),
+    url(r'^ask/.*$', 'qa.views.test', name='ask'),
+    url(r'^popular/.*$', 'qa.views.test', name='popular'),
+    url(r'^new/.*$', 'qa.views.test', name='new'),
     url(r'^admin/', include(admin.site.urls)),
 )
